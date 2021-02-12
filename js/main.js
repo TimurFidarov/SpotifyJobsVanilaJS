bdd5b25ba1ff268f3e05cced4217f253e49adea5
unknown <nowaybuticanfly@gmail.com>
2021-02-12 08:32:04 +0300

    Add fresh content interactive js part

--- js/main.js
+++ js/main.js
@@ -319,4 +319,65 @@ window.onload = function () {
             }, 500);
         }
     }
+
+
+
+
+    //drag elem
+
+    dragElement(document.getElementById("fresh-swiper"));
+
+    function dragElement(elmnt) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (document.getElementById(elmnt.id + "header")) {
+            /* if present, the header is where you move the DIV from:*/
+            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+        } else {
+            /* otherwise, move the DIV from anywhere inside the DIV:*/
+            elmnt.style.transition = 'none';
+            elmnt.onmousedown = dragMouseDown;
+        }
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            console.log(pos3);
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            // set the element's new position:
+            elmnt.style.transition = 'all 0ms ease 0s';
+
+            elmnt.style.transform = 'translate3d('+ -pos1*0.5 + "px,0px,0px)";
+        }
+
+        function closeDragElement() {
+            /* stop moving when mouse button is released:*/
+            document.onmouseup = null;
+            document.onmousemove = null
+            elmnt.style.transition = 'all 550ms ease 0s';
+            elmnt.style.transform = 'translate3d(0px,0px,0px)';
+        }
+    }
+
+
+    //go up
+
+    let goUpBtn = document.getElementById('go-up-btn');
+
+    goUpBtn.onclick = function () {
+        window.scrollTo({ top: 0, behavior: `smooth` })
+    };
+
+
 }
